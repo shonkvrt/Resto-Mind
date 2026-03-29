aeb03dac24a27fe85f912a4fb975a5cb47317d75
shonkvrt <shonkvrt@gmail.com>
2026-03-29 18:45:01 +0300

    feat: add Serialization and Deserialization

--- src/main/java/com/example/restomind/Ingredient.java
+++ src/main/java/com/example/restomind/Ingredient.java
@@ -2,12 +2,13 @@ package com.example.restomind;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-// To save the data in the hard disk we should implement Serializable
+/* implements Serializable because InventoryManager saves also Ingredient into the file
+    so it also has to implements Serializable*/
 public class Ingredient implements Serializable {
     private String name;
     private double amount;
     private double pricePerUnit;
-    private LocalDate expirationDate; // LocalDate is a date type that shows (day,month,year)
+    private LocalDate expirationDate; // localDate is a date type that shows (day,month,year)
 
     public Ingredient(String name, double amount, double pricePerUnit,LocalDate expirationDate){
         this.name = name;
--- src/main/java/com/example/restomind/InventoryManager.java
+++ src/main/java/com/example/restomind/InventoryManager.java
@@ -1,9 +1,10 @@
 package com.example.restomind;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
+// implements Serializable to save the object data in a file
 public class InventoryManager implements Serializable{
 
     private HashMap<String,Ingredient> ingredientsHashMap;
@@ -25,11 +26,11 @@ public class InventoryManager implements Serializable{
     // copy data for genetic algorithm
     public InventoryManager createCopy(){
         InventoryManager copy = new InventoryManager();
-        /* creates sets (String, Ingredient) of our data that we can go through all the
+        /* creates sets (String, Ingredient) of our data and then run through all the
          ingredients and copy them */
         for (Map.Entry<String,Ingredient> ingredient : ingredientsHashMap.entrySet()){
             Ingredient originalIngredient = ingredient.getValue();
-
+            // create a copy of the ingredient
             Ingredient copyIngredient = new Ingredient(
                     originalIngredient.getName(),
                     originalIngredient.getAmount(),
@@ -37,9 +38,35 @@ public class InventoryManager implements Serializable{
                     originalIngredient.getExpirationDate()
 
             );
-
+            // add the copy ingredient to the copy map
             copy.addIngredient(copyIngredient);
         }
         return copy;
+    }
+    // Serialization
+    // save the object data to a file so when you close the program the data won't disappear
+    public void saveDataToFile(String fileName) {
+        /* FileOutputStream connect to the specific file,
+         ObjectOutputStream takes care about transfer objects*/
+        try (ObjectOutputStream fileConnection = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            // takes the InventoryManager object and writes it into the file
+            fileConnection.writeObject(this);
+            System.out.println("Inventory saved successfully!");
+        } catch (IOException e) {
+            System.out.println("Inventory isn't saved, try again");
+            e.printStackTrace();
+        }
+    }
+    // Deserialization
+    // takes the object data from a specific file and return it
+    public static InventoryManager loadDataFromFile(String fileName) {
+        try (ObjectInputStream fileConnection = new ObjectInputStream(new FileInputStream(fileName))) {
+            return (InventoryManager) fileConnection.readObject();
+        }
+        /* if the class that had been written in the file doesn't exist the error will be caught
+           because it is ClassNotFoundException */
+        catch (IOException | ClassNotFoundException e) {
+            return new InventoryManager(); // returns empty object so it won't crash later
+        }
     }
 }
